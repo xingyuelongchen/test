5742552dada0c3c7b832dda613d5bccf01db3f5e
xingyuelongchen <xingyuelongchen@qq.com>
2019-04-18 11:45:58 +0800

    '更新BUG'

--- a/js/mian.js
+++ a/js/mian.js
@@ -4,7 +4,7 @@ function getEl(id) {
 
 
 //游戏时间
-var gameTime = 60;
+var gameTime = 30;
 
 
 // 开始游戏
@@ -32,6 +32,7 @@ function isTime() {
             clearInterval(timer);
             stop(); //游戏结束
             eTime.innerHTML = gameTime //重置时间
+            getEl('.fen').innerHTML = 0 //重置积分
         }
     }, 1000);
 
@@ -54,6 +55,9 @@ function jifen(a) {
 
         num = num * 1 + 1;
     }
+    if (!a && num > 0) {
+        num = num * 1 - 1;
+    }
     fen.innerHTML = num;
     fontColor();
     return num;
@@ -63,16 +67,16 @@ function jifen(a) {
 function fontColor() {
     var eText = getEl('.text');
     var eBtn = getEl('.btn').children;
-    var col = ['red', 'blue', 'yellow', 'green', 'black'];
-    var textCol = ['红', '蓝', '黄', '绿', '黑'];
-    var len = col.length; //固定的颜色长度
-    var btnlen = eBtn.length;
-    var random = randomNum(btnlen); //获取随机数
+    var col = ['red', 'blue', 'yellow', 'green', 'black',"purple"];
+    var textCol = ['红', '蓝', '黄', '绿', '黑','紫'];
+    var len = col.length; //获取的颜色长度
 
+    var random = randomNum(len); //获取颜色随机数
+    var btn = randomNum(eBtn.length);
     for (let i = 0; i < eBtn.length; i++) {
         let a = randomNum(len);
 
-        while (random == a) {
+        while (random == a) { //判断生成的随机数  是否跟答案重复,如有重复则重新生成
             a = randomNum(len);
         }
 
@@ -84,7 +88,8 @@ function fontColor() {
 
     eText.style.color = col[randomNum(len)]; //设置展示文本的颜色
     eText.innerHTML = textCol[random]; //设置展示的文字，
-    eBtn[random].style.color = col[random]; //随机给按钮添加正确答案的颜色
+    eBtn[btn].style.color = col[random]; //随机给按钮添加正确答案的颜色
+    eBtn[btn].index = random;   //设置正确答案，用于判断时候使用
 
     //判断是否选对
     isTrue(random);
@@ -93,12 +98,13 @@ function fontColor() {
 function isTrue(a) {
     var btns = getEl('.btn').children;
     for (let i = 0; i < btns.length; i++) {
-        btns[i].index = i;
         btns[i].onclick = function () {
             if (this.index == a) {
+                //答对加分
                 jifen(true);
             } else {
-                console.log('false')
+                //答错扣分
+                jifen(false);
             }
         }
     }
